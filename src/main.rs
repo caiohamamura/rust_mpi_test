503e52c158a04494eb644ffcd5e58058b3ea9efd
caioh <caioh@DESKTOP-RB1K0R8>
2019-04-03 12:28:47 -0300

    changed

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,38 @@
 extern crate mpi;
 
-use mpi::topology::Rank;
+use mpi::datatype::Partition;
 use mpi::traits::*;
+use mpi::Count;
 
 fn main() {
     let universe = mpi::initialize().unwrap();
     let world = universe.world();
-    let rank = world.rank() as usize;
-    let size = world.size() as usize;
-    let root_rank = 0i32;
-    let repeat = 4 as usize;
+
+    let rank = world.rank();
+    let size = world.size();
+
+    let root_rank = 0;
     let root_process = world.process_at_rank(root_rank);
 
-    let mut x = vec![0;repeat];
-    if rank == root_rank as usize {
-        let v = (0..size*repeat).collect::<Vec<_>>();
-        root_process.scatter_varcount_into_root(&v[..], &mut x[..]);
+    let mut buf = vec![0; rank as usize];
+
+    if rank == root_rank {
+        let msg: Vec<_> = (0..size).flat_map(|i| (0..i)).collect();
+        let counts: Vec<Count> = (0..size).collect();
+        let displs: Vec<Count> = counts
+            .iter()
+            .scan(0, |acc, &x| {
+                let tmp = *acc;
+                *acc += x;
+                Some(tmp)
+            })
+            .collect();
+        let partition = Partition::new(&msg[..], counts, &displs[..]);
+        root_process.scatter_varcount_into_root(&partition, &mut buf[..]);
     } else {
-        root_process.scatter_varcount_into(&mut x[..]);
+        root_process.scatter_varcount_into(&mut buf[..]);
     }
-    println!("{:?}", x);
+
+    assert!(buf.iter().zip(0..rank).all(|(&i, j)| i == j));
+    println!("Process {} got message: {:?}", rank, buf);
 }
