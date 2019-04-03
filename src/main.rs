35be4336ccb63043782409087d3572478e8fb15a
caioh <caioh@DESKTOP-RB1K0R8>
2019-04-03 11:56:49 -0300

    changed

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let world = universe.world();
     let rank = world.rank() as usize;
     let size = world.size() as usize;
-    let root_rank = 0;
+    let root_rank = 0usize;
     let repeat = 2 as usize;
     let root_process = world.process_at_rank(root_rank);
 
