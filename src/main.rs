ad1bdfe3bef2526d697d4eeef6771e2daf1a71e2
caioh <caioh@DESKTOP-RB1K0R8>
2019-04-03 11:53:25 -0300

    changed

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     let repeat = 2 as usize;
     let root_process = world.process_at_rank(root_rank);
 
-    let mut x = vec![0;repeat+1];
+    let mut x = vec![0;repeat*size];
     if rank == root_rank {
         let v = (0..size*repeat).collect::<Vec<_>>();
         root_process.scatter_into_root(&v[..], &mut x[..]);
