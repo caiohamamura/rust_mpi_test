af0eed6d462818af90da59602104b94ee08b5c06
caioh <caioh@DESKTOP-RB1K0R8>
2019-04-03 11:02:32 -0300

    changed

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,15 @@ fn main() {
 
     let rank = world.rank() as usize;
     let count = world.size() as usize;
-    let repeat : usize = 10;
-    let vec_res : Vec<usize> = (0..10).collect();
+    let repeat : usize = 2;
+    let vec_res = vec![false;repeat];
 
     if rank == 0 {
-        let mut a = vec![0usize; count*repeat];
+        let mut a = vec![false; count*repeat];
         world.process_at_rank(0).gather_into_root(&(vec_res)[..], &mut a[..]);
         println!("{:?}", a);
+    } else {
+        world.process_at_rank(0).gather_into(&(vec_res)[..]);
     }
     // let a : Vec<i32> = (0..100).collect();
     // println!("{:?}", a);
